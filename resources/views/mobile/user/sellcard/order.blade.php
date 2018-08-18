b035db6080c40ba16ca89c1d70a79db6360712fc
594433766@qq.com <cl105102049>
2018-08-18 17:38:37 +0800

    面对面办卡

--- resources/views/mobile/user/sellcard/order.blade.php
+++ resources/views/mobile/user/sellcard/order.blade.php
@@ -14,7 +14,7 @@
 						<div class="weui-cell__bd">
                             <p style="font-size: 14px;">订单号：{{ $value->order_sn }}</p>
                             <p style="font-size: 12px;color: #999;margin-top: 5px">时间：{{ $value->created_at->format('Y-m-d H:i') }}</p>
-							<p style="font-size: 12px;color: #999;margin-top: 5px">付款方式：{{ trans('user.sellcard.'.$value->pay_type) }}</p>
+							<p style="font-size: 12px;color: #999;margin-top: 5px">付款方式：{{ trans('user.sellcard.pay_type.'.$value->pay_type) }}</p>
 						</div>
                         <div class="weui-cell__ft">
                                 <strong style="color:#999999">{{ $value->pay_status ? '已付款' : '待付款' }}</strong>
