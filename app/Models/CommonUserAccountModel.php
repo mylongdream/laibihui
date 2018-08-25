62c1207a1a95736cbb31e9967a038a310f001556
594433766@qq.com <cl105102049>
2018-08-25 16:16:52 +0800

    后台账户记录

--- app/Models/CommonUserAccountModel.php
+++ app/Models/CommonUserAccountModel.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use HaoLi\LaravelAmount\Traits\AmountTrait;
 
 class CommonUserAccountModel extends Model
 {
+    use AmountTrait;
 
 	protected $table = 'common_user_account';
 	protected $primaryKey = 'id';
--- resources/views/mobile/user/promotion/first.blade.php
+++ resources/views/mobile/user/promotion/first.blade.php
@@ -21,26 +21,28 @@
                             </a>
                         </div>
                     </div>
-                    @foreach ($promotions as $value)
-                        <div class="weui-panel panel-item">
-                            <div class="weui-panel__bd">
-                                <a href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">
-                                    <div class="weui-media-box__hd">
-                                        <img class="weui-media-box__thumb" src="{{ $value->headimgurl ? uploadImage($value->headimgurl) : asset('static/image/common/getheadimg.jpg') }}" alt="">
-                                    </div>
-                                    <div class="weui-media-box__bd">
-                                        <h4 class="weui-media-box__title">{{ $value->username ? $value->username : '/' }}</h4>
-                                        <p class="weui-media-box__desc">手机：{{ $value->mobile ? $value->mobile : '/' }}</p>
-                                        <p class="weui-media-box__desc">时间：{{ $value->created_at->format('Y-m-d H:i') }}</p>
-                                    </div>
-                                </a>
+                    <div class="weui-tab__panel">
+                        @foreach ($promotions as $value)
+                            <div class="weui-panel panel-item">
+                                <div class="weui-panel__bd">
+                                    <a href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">
+                                        <div class="weui-media-box__hd">
+                                            <img class="weui-media-box__thumb" src="{{ $value->headimgurl ? uploadImage($value->headimgurl) : asset('static/image/common/getheadimg.jpg') }}" alt="">
+                                        </div>
+                                        <div class="weui-media-box__bd">
+                                            <h4 class="weui-media-box__title">{{ $value->username ? $value->username : '/' }}</h4>
+                                            <p class="weui-media-box__desc">手机：{{ $value->mobile ? $value->mobile : '/' }}</p>
+                                            <p class="weui-media-box__desc">时间：{{ $value->created_at->format('Y-m-d H:i') }}</p>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="weui-panel__ft">
+                                    <div class="z status">状态：{{ $value->card ? '已开卡' : '未开卡' }}</div>
+                                </div>
                             </div>
-                            <div class="weui-panel__ft">
-                                <div class="z status">状态：{{ $value->card ? '已开卡' : '未开卡' }}</div>
-                            </div>
-                        </div>
-                    @endforeach
-                    {!! $promotions->links() !!}
+                        @endforeach
+                        {!! $promotions->links() !!}
+                    </div>
                 </div>
             </div>
         </div>
