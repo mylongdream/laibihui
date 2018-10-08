8b4342e21c6c419c1b9a8570c1638def49c45afc
594433766@qq.com <cl105102049>
2018-10-08 12:43:59 +0800

    crm

--- app/Http/Controllers/Mobile/Crm/Shop/CommonController.php
+++ app/Http/Controllers/Mobile/Crm/Shop/CommonController.php
@@ -10,6 +10,14 @@ class CommonController extends Controller
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
+            $module = auth('crm')->user()->group->module;
+            if($module != 'shop'){
+                if ($request->ajax()){
+                    return response()->json(['status' => 0, 'info' => '你没有权限进入']);
+                }else{
+                    return response()->view('layouts.mobile.message', ['status' => 0, 'info' => '你没有权限进入']);
+                }
+            }
             $shop = auth('crm')->user()->shop;
             if(!$shop){
                 if ($request->ajax()){
