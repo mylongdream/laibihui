e588f2a21535c63c63ff6525568b565af969df03
594433766@qq.com <cl105102049>
2018-12-15 17:07:06 +0800

    修复bug

--- resources/views/mobile/crm/tuiguang/lackcard/checkin.blade.php
+++ resources/views/mobile/crm/tuiguang/lackcard/checkin.blade.php
@@ -8,7 +8,7 @@
                     <div class="back"><a href="javascript:history.go(-1);"><span></span></a></div>
                     <div class="nav">缺卡登记</div>
                 </div>
-                <form class="ajaxform" name="myform" method="post" action="{{ route('mobile.crm.shop.lackcard.checkin') }}">
+                <form class="ajaxform" name="myform" method="post" action="{{ route('mobile.crm.tuiguang.lackcard.checkin') }}">
                     {!! csrf_field() !!}
                     <div class="weui-article">
                         <p>尊敬的商户您好!请您按照实际用卡数量完成订卡申请，您的客户经理将免费为您送卡上门，谢谢</p>
@@ -23,7 +23,7 @@
                     </div>
                     <div class="weui-btn-area">
                         <button name="applybtn" type="button" class="weui-btn weui-btn_primary ajaxsubmit">立即预定</button>
-                        <a href="{{ route('mobile.crm.shop.lackcard.index') }}" class="weui-btn weui-btn_default">订卡记录</a>
+                        <a href="{{ route('mobile.crm.tuiguang.lackcard.index') }}" class="weui-btn weui-btn_default">订卡记录</a>
                     </div>
                 </form>
             </div>
