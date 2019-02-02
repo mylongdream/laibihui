b91cbe677fcfce94f4e5eaf30453731827198052
594433766@qq.com <cl105102049>
2019-02-02 19:10:16 +0800

    现金券

--- resources/views/user/coupon/index.blade.php
+++ resources/views/user/coupon/index.blade.php
@@ -9,25 +9,27 @@
 			<li class="{{ request('status') == 2 ? 'on' : '' }}"><a href="{{ route('user.coupon.index', ['status' => 2]) }}"><span>已失效</span></a></li>
 		</ul>
 	</div>
-	<div class="coupon-list mtw">
+	<div class="coupon-box cl">
 		@if (count($coupons))
-			@foreach ($coupons as $value)
-				<div class="coupon-item">
-					<div class="price"><em>¥</em><strong>{{ $value->coupon_amount }}</strong></div>
-					<div class="limit">{{ $value->coupon_fullamount ? '满'.$value->coupon_fullamount.'元可用' : trans('user.unlimit')}}</div>
-					<div class="time">
-						@if ($value->use_start && $value->use_end)
-							{{ $value->use_start->format('Y-m-d H:i') }} - {{ $value->use_end->format('Y-m-d H:i') }}
-						@elseif ($value->use_start)
-							{{ $value->use_start->format('Y-m-d H:i') }} - {{ trans('user.unlimit') }}
-						@elseif ($value->use_end)
-							{{ trans('user.unlimit') }} - {{ $value->use_end->format('Y-m-d H:i') }}
-						@else
-							{{ trans('user.unlimit') }}
-						@endif
+			<div class="coupon-list">
+				@foreach ($coupons as $value)
+					<div class="coupon-item">
+						<div class="price"><em>¥</em><strong>{{ $value->coupon_amount }}</strong></div>
+						<div class="limit">{{ $value->coupon_fullamount ? '满'.$value->coupon_fullamount.'元可用' : trans('user.unlimit')}}</div>
+						<div class="time">
+							@if ($value->use_start && $value->use_end)
+								{{ $value->use_start->format('Y-m-d H:i') }} - {{ $value->use_end->format('Y-m-d H:i') }}
+							@elseif ($value->use_start)
+								{{ $value->use_start->format('Y-m-d H:i') }} - {{ trans('user.unlimit') }}
+							@elseif ($value->use_end)
+								{{ trans('user.unlimit') }} - {{ $value->use_end->format('Y-m-d H:i') }}
+							@else
+								{{ trans('user.unlimit') }}
+							@endif
+						</div>
 					</div>
-				</div>
-			@endforeach
+				@endforeach
+			</div>
 		@else
 			<div class="nodata">暂无数据</div>
 		@endif
